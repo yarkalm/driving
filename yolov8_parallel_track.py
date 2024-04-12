9194e9da6ccb2f34d2806ccdb85d91ba198b41c9
Yaroslav Kalmykov <99351220+yarkalm@users.noreply.github.com>
2024-04-12 13:16:51 +0500

    Update yolov8_parallel_track.py. Change path weights

--- yolov8_parallel_track.py
+++ yolov8_parallel_track.py
@@ -17,8 +17,8 @@ title = 'YOLOv8 Tracking on cuda' if is_available() else 'YOLOv8 Tracking on cpu
 perspective_transformer()
 
 # Загрузка моделей YOLOv8
-car_model = YOLO('yolov8s.pt')
-sign_model = YOLO('roadsings_weights.pt')
+car_model = YOLO('weights/yolov8s.pt')
+sign_model = YOLO('weights/roadsings_weights.pt')
 
 cams = {'Доватора - Блюхера': "https://cdn.cams.is74.ru/hls/playlists/multivariant.m3u8?uuid=8e6a6c46-9f40-4f72-8c8c"
                               "-305ed67effdc",
